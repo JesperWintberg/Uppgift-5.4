c0b5a8f6d93866e0e45fc697d13dc58f529ba02a
Bolmgren1 <kebo05001@utb.vaxjo.se>
2023-01-20 10:39:05 +0100

    Add project files.

---
+++ Uppgift 5.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            namn[0] = "Sven";
+            namn[1] = "Ulla";
+            namn[2] = "Britt";
+            namn[3] = "Nils";
+            namn[4] = "Geraldin";
+
+            Array.Sort(namn);
+
+            Console.WriteLine($"Namen i alafbetsordning blir {namn[0]} {namn[1]} {namn[2]} {namn[3]} {namn[4]} ");
+        }
+    }
+}
+
